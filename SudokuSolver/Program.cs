f52d2d3e44302026412c53aef78606326480c669
Szymon Boniszewski <szymkop2001@gmail.com>
2022-11-09 01:01:14 +0100

    open file with basic error check

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,11 +1,55 @@
 ﻿namespace SudokuSolver
 {
+
     internal class Program
     {
+        static bool Check3x3(string data)
+        {
+            return true;
+        }
+
+        static bool CheckRow(string data)
+        {
+            return true;
+        }
+
+        static bool CheckColumn(string data)
+        {
+            return true;
+        }
+
+        static bool CheckIfCorrect(string data)
+        {
+            char[] correct = {'1','2','3','4','5','6','7','8','9','0','X'};
+            data = data.ToUpper();
+            foreach (char c in data)
+            {
+                if (!correct.Contains(c))
+                    return false;
+            }
+            if (data.Length != 81)
+                return false;
+
+            return true;
+        }
+
+        static string Open(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+
+            return sr.ReadToEnd();
+        }
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello, World!");
+            //string path = Console.ReadLine();
+            string data = Open(@"C:\Users\szymk\Desktop\kontrola.txt");
+            if(CheckIfCorrect(data))
+            {
+            }
+            else
+            {
+                Console.WriteLine("Errors in your data");
+            }
 
         }
     }
